c8d194b97724a59a02a4fd1225ca06b2db5bfe1f
Aljoscha Weber <aw@syndikat7.de>
2023-04-13 16:12:11 +0200

    task: fixes for env

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 /app/scripts/replace_env.sh replaceEnvVariable "API_HTTP_URI" "API_HTTP_URI" /app/dist
+/app/scripts/replace_env.sh replaceEnvVariable "LANGUAGE_HTTP_URI" "LANGUAGE_HTTP_URI" /app/dist
 
 exec nginx -g 'daemon off;'
